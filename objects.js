6e527f2d127f71b2c591821c6956f589cb52e39d
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 09:51:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { "Drake": "started from the bottom" };
 
 function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {artist: song});
+  return Object.assign({}, playlist, {[artist]: song});
 }
 
 function removeFromPlaylist(playlist, artist) {
