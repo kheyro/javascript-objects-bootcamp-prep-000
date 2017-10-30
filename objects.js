89aead595eb8bf2362c6b9c6f63c9a19cad342e7
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 09:54:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  return delete playlist.artist;
+  delete playlist.artist;
+  return playlist;
 }
